2f42f49296aa65b33122065d14367fc29f8fccaf
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-11-18 12:28:58 +0800

    Add Form reducer

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,38 +1,99 @@
 /* eslint-disable react/prop-types */
+import { useReducer } from 'react';
 import Button from './Button';
 import FormInput from './FormInput';
 
+const initialState = {
+  first: '',
+  last: '',
+  email: '',
+  query: '',
+  message: '',
+  consent: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'setFirst':
+      return { ...state, first: action.payload };
+    case 'setLast':
+      return { ...state, last: action.payload };
+    case 'setEmail':
+      return { ...state, email: action.payload };
+    case 'setQuery':
+      return { ...state, query: action.payload };
+    case 'setMessage':
+      return { ...state, message: action.payload };
+    case 'setConsent':
+      return { ...state, consent: action.payload };
+    default:
+      return 'Action not found';
+  }
+}
+
 function Form({ title }) {
+  const [state, dispatch] = useReducer(reducer, {});
+  const { first, last, email, message, consent } = state;
+
+  function handleDispatch(type, payload) {
+    dispatch({ type, payload });
+  }
+
   return (
-    <div className='grid bg-white grid-cols-1 sm:grid-cols-2 mx-4 my-8 p-6 rounded-2xl gap-6 md:min-w-[700px] shadow-sm'>
+    <div className='grid bg-white grid-cols-1 sm:grid-cols-2 mx-4 my-8 p-6 sm:p-8 rounded-2xl gap-6 md:min-w-[700px] shadow-sm'>
       <h1 className='sm:col-span-2 text-3xl font-bold'>{title}</h1>
 
       {/* first name */}
       <div className='grid sm:col-span-1 gap-2'>
-        <label htmlFor='first'>First Name</label>
-        <FormInput type='text' id='first' />
+        <label htmlFor='first'>
+          First Name <span className='text-green-600'>*</span>
+        </label>
+        <FormInput
+          onChange={e => handleDispatch('setFirst', e.target.value)}
+          value={first}
+          type='text'
+          id='first'
+        />
       </div>
 
       {/* last name */}
       <div className='grid sm:col-span-1 gap-2'>
-        <label htmlFor='last'>Last Name</label>
-        <FormInput type='text' id='last' />
+        <label htmlFor='last'>
+          Last Name <span className='text-green-600'>*</span>
+        </label>
+        <FormInput
+          onChange={e => handleDispatch('setLast', e.target.value)}
+          value={last}
+          type='text'
+          id='last'
+        />
       </div>
 
-      {/* email name */}
+      {/* email */}
       <div className='grid sm:col-span-2 gap-2'>
-        <label htmlFor='email'>Email</label>
-        <FormInput type='text' id='email' />
+        <label htmlFor='email'>
+          Email <span className='text-green-600'>*</span>
+        </label>
+        <FormInput
+          onChange={e => handleDispatch('setEmail', e.target.value)}
+          value={email}
+          type='text'
+          id='email'
+        />
       </div>
 
       {/* query type */}
       <div className='grid sm:col-span-2 gap-4'>
-        <label htmlFor='query'>Query Type</label>
+        <label htmlFor='query'>
+          Query Type <span className='text-green-600'>*</span>
+        </label>
 
         <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
           {/* apply selected radio bg here */}
           <div className='grid grid-cols-[auto_1fr] gap-4 border border-grey-500 rounded-lg px-5 py-3 hover:cursor-pointer '>
             <input
+              onChange={e => handleDispatch('setQuery', e.target.value)}
+              value='general'
               type='radio'
               name='query'
               id='general'
@@ -46,6 +107,8 @@ function Form({ title }) {
           {/* apply selected radio bg here */}
           <div className='grid grid-cols-[auto_1fr] gap-4 border border-grey-500 rounded-lg px-5 py-3 hover:cursor-pointer'>
             <input
+              onChange={e => handleDispatch('setQuery', e.target.value)}
+              value='support'
               type='radio'
               name='query'
               id='support'
@@ -60,12 +123,15 @@ function Form({ title }) {
 
       {/* message */}
       <div className='grid sm:col-span-2 gap-2'>
-        <label htmlFor='message'>Message</label>
+        <label htmlFor='message'>
+          Message <span className='text-green-600'>*</span>
+        </label>
         <textarea
+          onChange={e => handleDispatch('setMessage', e.target.value)}
+          value={message}
           name='message'
           id='message'
-          // rows='5'
-          className='border border-grey-500 rounded-lg px-5 py-3 resize-y h-36 sm:h-24 hover:border-green-600 transition-all cursor-pointer focus:outline-green-600 outline-1'
+          className='border border-grey-500 rounded-lg px-5 py-3 resize-y min-h-36 sm:min-h-24 hover:border-green-600 transition-all cursor-pointer focus:outline-green-600 outline-1'
         ></textarea>
       </div>
 
@@ -73,13 +139,16 @@ function Form({ title }) {
       <div className='grid sm:col-span-2 my-4'>
         <div className='grid grid-cols-[auto_1fr] gap-4 '>
           <input
+            onChange={e => handleDispatch('setConsent', e.target.checked)}
+            value={consent}
             type='checkbox'
             name='consent'
             id='consent'
             className='accent-green-600 border-grey-500 hover:border-green-600 transition-all focus:outline-green-600 outline-1  hover:cursor-pointer'
           />
           <label htmlFor='consent' className='hover:cursor-pointer'>
-            I consent to being contacted by the team
+            I consent to being contacted by the team{' '}
+            <span className='text-green-600'>*</span>
           </label>
         </div>
       </div>
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 function FormInput({ ...props }) {
-  const { type, name, id, value } = props;
+  const { type, name, id, value, onChange } = props;
 
   return (
     <input
@@ -8,7 +8,8 @@ function FormInput({ ...props }) {
       name={name}
       id={id}
       value={value}
-      className='border border-grey-500 rounded-lg px-5 py-3 hover:border-green-600 transition-all cursor-pointer focus:outline-green-600 outline-1'
+      onChange={onChange}
+      className='border border-grey-500 rounded-lg px-5 py-3 hover:border-green-600 transition-all cursor-pointer focus:outline-green-600 outline-1 min-w-full'
     />
   );
 }
